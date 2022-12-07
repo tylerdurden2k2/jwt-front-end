e0dbbc4134b10e7053b0e28d447a6f70680f7cb9
Quang Huy <phamquanghuy2809@gmail.com>
2022-12-07 21:59:59 +0700

    set-cookie

--- src/setup/axios.js
+++ src/setup/axios.js
@@ -3,6 +3,7 @@ const instance = axios.create({
     baseURL: "http://localhost:8080",
 });
 // Add a request interceptor
+instance.defaults.withCredentials = true;
 instance.interceptors.request.use(
     function (config) {
         // Do something before request is sent
